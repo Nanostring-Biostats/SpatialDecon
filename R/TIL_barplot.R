e7f0610d1768478ece208cadd90a61a9752361c4
patrickjdanaher <patrickjdanaher@gmail.com>
2020-07-28 10:43:04 -0700

    make TIL_Barplot compatible with non-safeTME input

--- R/TIL_barplot.R
+++ R/TIL_barplot.R
@@ -21,6 +21,7 @@
 #'  whatever order is best for display.
 #' @param draw_legend Logical. If TRUE, the function draws a legend in a new plot frame.
 #' @param main Title for barplot
+#' @param col Vector of colors for cell types. Defaults to pre-set colors for the safeTME cell types. 
 #' @param ... Arguments passed to barplot()
 #' @examples
 #' data(mini_geomx_dataset)
@@ -41,13 +42,35 @@
 #' # run barplot and draw a color legend
 #' TIL_barplot(mat = res0$beta, draw_legend = TRUE)
 #' @export
-TIL_barplot <- function(mat, draw_legend = FALSE, main = "", ...) {
-  usecells <- intersect(rownames(mat), names(SpatialDecon::cellcols))
+TIL_barplot <- function(mat, draw_legend = FALSE, main = "", col = NULL, ...) {
 
+  
+  # infer colors:
+  if (length(col) == 0) {
+    # use safeTME colors if the right cells are present:
+    if (all(is.element(rownames(mat), names(SpatialDecon::cellcols)))) {
+      col <- SpatialDecon::cellcols[rownames(mat)]
+    }
+    else {
+      manycols <- c(
+        "#8DD3C7", "#FFFFB3", "#BEBADA", "#FB8072", "#80B1D3", "#FDB462", "#B3DE69",
+        "#FCCDE5", "#A6CEE3", "#1F78B4", "#B2DF8A", "#33A02C", "#FB9A99", "#E31A1C",
+        "#FDBF6F", "#FF7F00", "#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E",
+        "#E6AB02", "#A6761D", "#666666", sample(grDevices::colors(), 99)
+      )
+      col <- manycols[1:nrow(mat)]
+      names(col) <- rownames(mat)
+    }
+  }
+  
+  
+  #  usecells <- intersect(rownames(mat), names(SpatialDecon::cellcols))
+  usecells <- rownames(mat)
+  
   # draw barplot:
   graphics::barplot(mat[usecells, ],
     cex.lab = 1.5,
-    col = SpatialDecon::cellcols[rownames(mat)], border = NA,
+    col = col, border = NA,
     las = 2, main = main, ...
   )
 
@@ -55,8 +78,8 @@ TIL_barplot <- function(mat, draw_legend = FALSE, main = "", ...) {
   if (draw_legend) {
     graphics::frame()
     graphics::legend("center",
-      fill = rev(SpatialDecon::cellcols[usecells]),
-      legend = rev(usecells)
+      fill = rev(col),
+      legend = rev(names(col))
     )
   }
 }
